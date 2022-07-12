214ba88650731508a05e7c2973eeb64d46fe641d
James Wallace <jawallace0392@gmail.com>
2022-07-12 00:45:02 -0400

    added my first test as practice

---
+++ src/test/java/cydeo/Test.java
@@ -0,0 +1,33 @@
+package cydeo;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.bytebuddy.asm.Advice;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+public class Test {
+    @Then("User is redirected to the results")
+    public void userIsRedirectedToTheResults() {
+        Assert.assertTrue(Driver.getDriver().getTitle().contains("Apple"));
+    }
+
+    @Given("User is on google home page")
+    public void userIsOnGoogleHomePage() {
+        Driver.getDriver().get("https://www.google.com");
+    }
+
+    @When("User google enters apple into search bar")
+    public void userGoogleEntersAppleIntoSearchBar() {
+        Driver.getDriver().findElement(By.name("q")).sendKeys("apple");
+
+    }
+
+    @And("User clicks enter")
+    public void userClicksEnter() {
+        Driver.getDriver().findElement(By.name("q")).sendKeys(Keys.ENTER);
+    }
+}
---
+++ src/test/resources/features/test.feature
@@ -0,0 +1,7 @@
+Feature: Google Search
+
+  Scenario: User Searches on google
+    Given User is on google home page
+    When User google enters apple into search bar
+    And User clicks enter
+    Then User is redirected to the results
